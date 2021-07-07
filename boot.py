1576b520eb60879e1131e615081bdeeef7ddce6a
Orion Serup <48197072+orionserup@users.noreply.github.com>
2021-07-07 02:03:11 -0700

    check

--- boot.py
+++ boot.py
@@ -18,6 +18,7 @@ class ESP8266:
     WLAN = None
     UART = None
 
+    # Connects to the Wifi
     def InitWifi(self, SSID=WIFISSID, PASS=WIFIPASS):
         
         self.WIFISSID = SSID
@@ -30,14 +31,21 @@ class ESP8266:
         while not self.WLAN.isconnected():
             self.WLAN.connect(input("WI-FI SSID"), input("WI-FI Password")
         
+    # Intializes The UART and gets it ready to send
     def InitSerial(self, PORT=1, SPEED=9600):
 
         self.UART = machine.UART(PORT, baudrate=SPEED)
 
+    # Sends a string over UART
+    def SendSerial(self, string):
+        if self.UART is not None:
+            self.UART.write((str)string)
 
+    # When An Object is Created Initialize the Wifi and Serial
     def __init__():
         InitWifi()
         InitSerial()
 
+# If running this as a module, create an object and initialize everything
 if __name__ == "__main__":
     esp = ESP8266()
